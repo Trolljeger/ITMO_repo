1810d433e6a8bb2f7a39dba5a95a162b05042c39
DmitriyMukhin <dmitry.a.mukhin@gmail.com>
2023-01-24 23:36:26 +0300

    Домашнее задание №3

---
+++ main.py
@@ -0,0 +1,28 @@
+#===================
+# Task 1
+
+str1 = "Python is the best programming language in the world"
+print(str1[6:-7], "\n")
+
+#===================
+# Task 2
+
+str2 = "Guido van Rossum is the benevolent dictator for life"
+print(str2[2::3], "\n")
+
+#===================
+# Task 3
+
+str3 = "You have a problem with authority, Mr. Anderson."
+print(str3)
+
+char_set = set(str3) #����� ���������� �������� � ������
+#print(char_set)
+#print(list(map(str3.count, char_set)))
+
+dctr = dict(zip(char_set, map(str3.count, char_set)))
+print(dctr)
+
+
+
+
